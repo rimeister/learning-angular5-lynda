cc65d46738e017b86a14aaf3078d708b524b2268
Riley Boyd <contact@rileyboyd.com>
2018-05-26 16:06:12 -0400

    Chapter 2, video 1 complete -- should have labelled previous chapter 'chapter 1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', // Identifies a custom html tag, where the component will be installed, found in index.html
-  template: `
-	<div class="row justify-content-center">
-	  <div class="card mt-sm-3 col-md-8">
-	    <div class="card-body">
-	      <h3 class="mb-0">Artist Directory</h3>
-	      <div class="form-group">
-	        <div class="form-label"></div>
-	        <input class="form-control mt-2" type="text">
-	      </div><!-- form-group -->
-	    </div><!-- card-body -->
-	  </div><!-- card -->
-	</div><!-- row -->
-  `, // Points to html template
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html' // Points to html template
 }) // Don't put semicolon here, will give error
 
 export class AppComponent {
